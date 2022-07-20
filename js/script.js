c6cad028a8730bdda359e2fbff916c723e94e899
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-21 02:21:19 +0600

    js code

--- js/script.js
+++ js/script.js
@@ -10,12 +10,23 @@ window.onload = () => {
   main();
 };
 
-function main() {}
+function main() {
+  const root = document.getElementById(`root`);
+  const changeBtn = document.getElementById(`change-btn`);
+
+  changeBtn.addEventListener(`click`, function () {
+    const bgColor = generateRGBColor();
+    root.style.backgroundColor = bgColor; 
+  });
+}
 
 // Step-02: Random color generator function.
 function generateRGBColor() {
-   const red = Math.floor(Math.random() * 255);
-   console.log(red);
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  return `rgb(${red}, ${green},${blue})`;
 }
 
 //  Step-03: collect all necessary references.
